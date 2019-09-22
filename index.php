a6fbe5ef115c3ceb0165c41077f23a19ab47d957
FMTW <42906455+FMTW@users.noreply.github.com>
2019-09-23 04:17:43 +1000

    Article Class, Home Page, Footer, Navigation
    Added an Article class, it only pull text_content so far, need to implement the video and images later. Also need to add a new function to addArticle.
    
    Created a layout for home page and make sure bootstrap working fine with twig etc.
    
    Added a simple footer with copyright.
    
    Modified the Navigation to fit container width, just so it looks better on smaller device.
    
    NOTE: THE LAYOUT IS NOT WORKING ON MOBILE!!!

--- index.php
+++ index.php
@@ -1,11 +1,17 @@
 <?php
 require('vendor\autoload.php');
 
+use DevBlog\Article;
+
+$article = new Article();
+$articleResult = $article->getArticle();
+
 $loader = new Twig_Loader_Filesystem("templates");
 $twig = new Twig_Environment($loader);
 $template = $twig->load('home.twig');
 
 echo $template->render([
-    'title' => 'Dev-Blog'
+    'title' => 'Dev-Blog',
+    'articles' => $articleResult
 ]);
 ?>
