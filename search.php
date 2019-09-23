f9ee35207b23075a88efd6bd9e7baf187b39da5e
Xuan6961 <git@github.com>
2019-09-23 21:58:19 +1000

    add search bar

---
+++ search.php
@@ -0,0 +1,34 @@
+<?php
+require('vendor/autoload.php');
+
+use DevBlog\Search;
+
+if( isset($_GET['query']) ){
+  $search = new Search();
+  $result = $search -> getSearchResult();
+}
+else{
+  echo "no parameter set";
+  $result = '';
+}
+
+// create navigation
+// use aitsydney\Navigation;
+
+// $nav = new Navigation();
+// $navigation = $nav -> getNavigation();
+
+//create twig loader for templates
+$loader = new Twig_Loader_Filesystem('templates');
+//create twig environment and pass the loader
+$twig = new Twig_Environment($loader);
+//call a twig template
+$template = $twig -> load('search.twig');
+
+//output the template and pass the data
+echo $template -> render( array(
+  'result' => $result,
+  'title' => "Search Result for " . $result['query']
+) );
+
+?>
